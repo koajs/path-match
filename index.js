c26c6b3f6e40a7b005e1a68ce064cbfed08eda72
Jonathan Ong <jonathanrichardong@gmail.com>
2015-12-23 22:19:03 -0800

    remove options.end option

--- index.js
+++ index.js
@@ -12,31 +12,12 @@ module.exports = function (options) {
     if (arguments.length > 2) fn = [].slice.call(arguments, 1)
     if (Array.isArray(fn)) fn = compose(flatten(fn))
 
-    if (options.end !== false) return function* (next) {
+    return function* (next) {
       const params = match(this.request.path, this.params)
       if (!params) return yield* next
 
       this.params = params
       yield* fn.call(this, next)
     }
-
-    throw new Error('not implemented')
-
-    // return function* (next) {
-    //   const params = match(this.request.path, this.params)
-    //   if (!params) return yield* next
-    //
-    //   this.params = params
-    //
-    //   const prefix = this.mountPath
-    //   const prev = this.request.path
-    //   const newpath = this.request.path =
-    //   yield* fn.call(this, function* () {
-    //     this.request.path = prev
-    //     yield* next
-    //     this.request.path = newpath
-    //   }.call(this))
-    //   this.request.path = prev
-    // }
   }
 }
